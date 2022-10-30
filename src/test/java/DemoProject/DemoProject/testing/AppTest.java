c8bca18b2a5d9f3659867a40b876baad8ee37325
learnanytime2021 <learnanytime2021@gmail.com>
2022-10-30 13:09:10 +0530

    modified class and added new class

--- src/test/java/DemoProject/DemoProject/testing/AppTest.java
+++ src/test/java/DemoProject/DemoProject/testing/AppTest.java
@@ -7,6 +7,7 @@ public class AppTest {
 	@Test
 	public void test() {
 		System.out.println("test");
+		System.out.println("demo");
 	}
 	
 }
---
+++ src/test/java/DemoProject/DemoProject/testing/Demo.java
@@ -0,0 +1,10 @@
+package DemoProject.DemoProject.testing;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+public void testCase() {
+	System.out.println("demo");
+	}
+}
